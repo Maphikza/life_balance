4c9ed427a0a096630eb80d97b51de26d955485a6
Maphikza <stapisi@protonmail.com>
2023-03-13 12:19:45 +0200

    making logo name bold on header

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import json
 import time
 
 path = Path(r"C:\Users\stapi\PycharmProjects\life_scale\instance\living.db")
-COMPANY_NAME = "LifePath"
+COMPANY_NAME = "LifePathMate"
 now = datetime.now()
 current_month = now.month
 
